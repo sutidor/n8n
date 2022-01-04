31375a105406f442bb5855177e85d52ef4872f86
Sven <34579211+sutidor@users.noreply.github.com>
2022-01-04 23:47:00 +0100

    Update install-custom-modules.sh

--- docker/images/n8n/install-custom-modules.sh
+++ docker/images/n8n/install-custom-modules.sh
@@ -4,9 +4,20 @@ if [ "$OPENSSH" = true ] ; then
   apk update && apk add openssh openssh-keygen expect
 	mkdir /home/node/.ssh && chown node:node /home/node/.ssh && chmod 2700 /home/node/.ssh
 fi
-# apk --virtual build-dependencies add python make g++ bash
+
 CUSTOM_MODULE_DIR="/home/node/.n8n/custom"
 CUSTOM_FUNCTION_DIR="/usr/local/lib"
+
+if [ ! -z "$CUSTOM_APK" ]
+then
+    LIST=(${CUSTOM_APK//;/ })
+		apk update
+    for module in "${LIST[@]}"; do
+        echo "custom apk: ${module}"
+        apk add ${module}
+    done
+		rm -rf /var/cache/apk/*
+fi
 
 # Create custom extension folder if it is not present
 if [ ! -z "$N8N_CUSTOM_EXTENSIONS" ]
@@ -21,40 +32,39 @@ then
 else
     if [[ ! -d "$CUSTOM_MODULE_DIR" ]]
     then
-				ls -al /home/node
         mkdir "$CUSTOM_MODULE_DIR"
     fi
 fi
 
-# Install custom modules
+# Install custom modules; --prefix does not work
 if [ ! -z "$CUSTOM_MODULES" ]
 then
     LIST=(${CUSTOM_MODULES//;/ })
+		cd $CUSTOM_MODULE_DIR
     for module in "${LIST[@]}"; do
         echo "custom module: ${module}"
-        npm i --prefix $CUSTOM_MODULE_DIR ${module}
+        npm i ${module}
     done
 fi
 
-# Install custom modules using --unsafe-perm flag
+# Install custom modules using --unsafe-perm flag; --prefix does not work
 if [ ! -z "$CUSTOM_MODULES_UNSAFEPERM" ]
 then
     LIST=(${CUSTOM_MODULES_UNSAFEPERM//;/ })
+		cd $CUSTOM_MODULE_DIR
     for module in "${LIST[@]}"; do
         echo "custom module unsafeperm: ${module}"
-        npm i --prefix $CUSTOM_MODULE_DIR ${module} --unsafe-perm
+        npm i ${module} --unsafe-perm
     done
 fi
 
-# Install allowed external functions
+# Install allowed external functions; --prefix does not work
 if [ ! -z "$NODE_FUNCTION_ALLOW_EXTERNAL" ]
 then
     LIST=(${NODE_FUNCTION_ALLOW_EXTERNAL//,/ })
+		cd $CUSTOM_FUNCTION_DIR
     for module in "${LIST[@]}"; do
         echo "custom external function: ${module}"
-        npm i --prefix $CUSTOM_FUNCTION_DIR ${module}
+        npm i ${module}
     done
 fi
-
-#apk del build-dependencies
-#rm -rf /var/cache/apk/*
