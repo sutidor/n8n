27d92fdf881eccda7ac12f8cec5712b251b66cc1
Sven <34579211+sutidor@users.noreply.github.com>
2022-01-04 21:34:40 +0100

    Update install-custom-modules.sh

--- docker/images/n8n/install-custom-modules.sh
+++ docker/images/n8n/install-custom-modules.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-apk update && apk add openssh openssh-keygen expect
-mkdir /home/node/.ssh && chown node:node /home/node/.ssh && chmod 2700 /home/node/.ssh
 
-apk --virtual build-dependencies add python make g++ bash
+if $OPENSSH; then
+  apk update && apk add openssh openssh-keygen expect
+	mkdir /home/node/.ssh && chown node:node /home/node/.ssh && chmod 2700 /home/node/.ssh
+fi
+# apk --virtual build-dependencies add python make g++ bash
 CUSTOM_MODULE_DIR="/home/node/.n8n/custom"
 CUSTOM_FUNCTION_DIR="/usr/local/lib"
 
+# Create custom extension folder if it is not present
 if [ ! -z "$N8N_CUSTOM_EXTENSIONS" ]
 then
     LIST=(${N8N_CUSTOM_EXTENSIONS//;/ })
@@ -22,6 +25,7 @@ else
     fi
 fi
 
+# Install custom modules
 if [ ! -z "$CUSTOM_MODULES" ]
 then
     LIST=(${CUSTOM_MODULES//;/ })
@@ -31,6 +35,7 @@ then
     done
 fi
 
+# Install custom modules using --unsafe-perm flag
 if [ ! -z "$CUSTOM_MODULES_UNSAFEPERM" ]
 then
     LIST=(${CUSTOM_MODULES_UNSAFEPERM//;/ })
@@ -40,6 +45,7 @@ then
     done
 fi
 
+# Install allowed external functions
 if [ ! -z "$NODE_FUNCTION_ALLOW_EXTERNAL" ]
 then
     LIST=(${NODE_FUNCTION_ALLOW_EXTERNAL//,/ })
